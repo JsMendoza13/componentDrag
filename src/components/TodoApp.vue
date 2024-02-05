743dc05fde4a0f1c4307822d55606ec3ebb10ad0
Jesus Mendoza Martinez <jsmendoza1312@gmail.com>
2024-02-05 16:42:50 -0500

    end component

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,5 +1,154 @@
-<script setup></script>
+<script setup>
+import { reactive } from "vue";
+import InputNew from "./InputNew.vue";
 
-<template><h1>hello Js</h1></template>
+let boards = reactive([
+  {
+    id: crypto.randomUUID(),
+    name: "tablero 1",
+    items: [
+      {
+        id: crypto.randomUUID(),
+        title: "Feature de archivos",
+      },
+      {
+        id: crypto.randomUUID(),
+        title: "Resolver bug",
+      },
+    ],
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "tablero 2",
+    items: [
+      {
+        id: crypto.randomUUID(),
+        title: "Mandar reporte",
+      },
+      {
+        id: crypto.randomUUID(),
+        title: "Code review",
+      },
+    ],
+  },
+]);
 
-<style scoped></style>
+function handleNewItem(text, board) {
+  board.items.push({
+    id: crypto.randomUUID(),
+    title: text.value,
+  });
+}
+function handleNewBoard() {
+  const name = prompt("Name of the board");
+  if (!!name) {
+    boards.push({
+      id: crypto.randomUUID(),
+      name: name,
+      items: [],
+    });
+  }
+}
+
+function startDrag(evt, board, item) {
+  evt.dataTransfer.setData(
+    "text/plain",
+    JSON.stringify({
+      boardId: board.id,
+      itemId: item.id,
+    })
+  );
+}
+
+function onDrag(evt, dest) {
+  const { boardId, itemId } = JSON.parse(
+    evt.dataTransfer.getData("text/plain")
+  );
+  const originBoard = boards.find((item) => item.id == boardId);
+  const originItem = originBoard.items.find((item) => item.id == itemId);
+
+  dest.items.push({ ...originItem });
+  originBoard.items = originBoard.items.filter((item) => item != originItem);
+}
+</script>
+
+<template>
+  <nav class="nav">
+    <ul>
+      <li><a href="#" @click.prevent="handleNewBoard">Create board</a></li>
+    </ul>
+  </nav>
+
+  <div class="boards-container">
+    <div class="boards">
+      <div
+        class="board"
+        @drop="onDrag($event, board)"
+        @dragover.prevent
+        @dragenter.prevent
+        v-for="board in boards"
+        :key="board.id"
+      >
+        <div>{{ board.name }}</div>
+        <InputNew @on-new-item="(text) => handleNewItem(text, board)" />
+        <div class="items">
+          <div
+            class="item"
+            draggable="true"
+            @dragstart="startDrag($event, board, item)"
+            v-for="item in board.items"
+            :key="item.id"
+          >
+            {{ item.title }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.nav {
+  background-color: rgba(0, 0, 0, 0.479);
+  margin-bottom: 10px;
+  padding: 15px;
+  border-radius: 8.5px;
+}
+
+.nav ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+}
+
+.nal ul li a {
+  display: block;
+  padding: 10px;
+  color: antiquewhite;
+  text-decoration: none;
+}
+.boards {
+  display: flex;
+  gap: 10px;
+}
+
+.board {
+  background: #efefef91;
+  padding: 10px;
+  border-radius: 10.5px;
+}
+
+.items {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+}
+
+.item {
+  background-color: rgb(160, 160, 160);
+  padding: 10px;
+  box-sizing: border-box;
+  border-radius: 5.5px;
+}
+</style>
